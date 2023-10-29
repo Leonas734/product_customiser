d5767178afd12e3511a186db24e7305e1e070374
Leonas734 <leonasadomaitis@gmail.com>
2023-10-29 13:59:38 +0000

    main.js refactor & basic layout added
    index.html & style.css basic layout added.
    main.js:
    - Added new functions for cleaner code
    - Added a new functio ncalled ResetView that puts the object to the center of the screen for the user. This functionc an be used later together with the "Reset View" button.

--- javascript/main.js
+++ javascript/main.js
@@ -3,25 +3,21 @@ import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
+const SCREEN_PERCENTAGE = .7;
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-//Keep track of the mouse position, so we can make the eye move
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
-
-let object;
+const camera = new THREE.PerspectiveCamera( 75, (window.innerWidth * SCREEN_PERCENTAGE) / window.innerHeight, 0.1, 1000 );
 
 let controls;
 
 const loader = new GLTFLoader();
 
+
 loader.load(
     `models/scene.gltf`,
     function (gltf) {
       //If the file is loaded, add it to the scene
-      object = gltf.scene;
-      scene.add(object);
+      scene.add(gltf.scene);
     },
     function (xhr) {
       //While it is loading, log the progress
@@ -35,41 +31,38 @@ loader.load(
 
 //Instantiate a new renderer and set its size
 const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth * SCREEN_PERCENTAGE, window.innerHeight);
 
 //Add the renderer to the DOM
 document.getElementById("container3D").appendChild(renderer.domElement);
 
-//Set how far the camera will be from the 3D model
-camera.position.set(-3, 3, 7.5);
+AddResizeListener()
+ResetView(); 
+Animate(); // Start 3D rendering
 
-//camera.up.set(0,1,0);
-//camera.rotation.set(20,0,0);
 
-controls = new OrbitControls(camera, renderer.domElement);
-
-// controls.lookAt(new THREE.Vector3(0, 0, 0));
-
-//Render the scene
-function animate() {
-    requestAnimationFrame(animate);
-    //Here we could add some code to update the scene, adding some automatic movement
+function Animate() {
+    requestAnimationFrame(Animate);
     renderer.render(scene, camera);
-  }
-  
-  //Add a listener to the window, so we can resize the window and the camera
-  window.addEventListener("resize", function () {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  });
-  
-  //add mouse position listener, so we can make the eye move
-  document.onmousemove = (e) => {
-    console.log(camera);
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-  }
-  
-  //Start the 3D rendering
-  animate();
+}
+
+function ResetView()
+{
+    //Set how far the camera will be from the 3D model
+    camera.position.set(0, 3, 10);
+
+    // Update rotation
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 2.25, 0);
+    controls.update();
+}
+
+function AddResizeListener()
+{
+    //Add a listener to the window, so we can resize the window and the camera
+    window.addEventListener("resize", function () {
+        camera.aspect = window.innerWidth * SCREEN_PERCENTAGE / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth * SCREEN_PERCENTAGE, window.innerHeight);
+    });
+}
