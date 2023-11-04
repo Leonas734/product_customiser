588ce80ec991189abf3288fd2fd766c042fbef26
Leonas734 <leonasadomaitis@gmail.com>
2023-11-04 14:47:49 +0000

    Vite satic build configuration added & README updt
    1. Updated README.md
    2. Added a build configuration for vite so it can easily be served
    locally or to github pages.
    3. README.md contains all the information needed.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+require('dotenv').config()
 
+
+const { PRODUCTION } = process.env
+const build = process.env == "true"
 export default defineConfig({
-  build: {
+    // used for building - "base" is the name of the github repo.
+    // easy guide here https://dev.to/shashannkbawa/deploying-vite-app-to-github-pages-3ane
+    base: build ? "/product_customiser/" : "",
+    build: {
     rollupOptions: {
       input: {
         app: './html/product-customiser/index.html',
